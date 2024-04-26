e5ecf02bd0b6413ec1aca01a52375c9877d6278a
Ananya Misra <anumisra2@gmail.com>
2024-04-26 08:18:32 +0530

    main screem implementataion

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import "./index.css";
+import App from "./App";
+
+//React hooks to a div to popultate content.
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
