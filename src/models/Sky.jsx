60400e23615792d9cc5fdae8bdd267a0d96117f7
Ananya Misra <ananya.misra@unthinkable.co>
2024-05-12 14:17:28 +0530

    modifications to the plane mesh

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 import skyScene from '../assets/3d/sky.glb'
 import { useGLTF } from '@react-three/drei'
-const Sky = () => {
-    const sky =useGLTF(skyScene)
+const Sky = ({isRotating}) => {
+   const skyRef=useRef();
+    const sky =useGLTF(skyScene);
+
+    useFrame((_,delta)=>{
+      if(isRotating)
+      {
+        skyRef.current.rotation.y +=0.25 * delta
+      }
+
+    })
   return (
-  <mesh>
+  <mesh ref={skyRef}>
     <primitive object={sky.scene}/>
-    
   </mesh>
   )
 }
